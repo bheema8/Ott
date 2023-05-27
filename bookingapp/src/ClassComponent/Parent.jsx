d7c2e5f3d840c22194edae769ef110ebd5b79012
bheema8 <rahulbheema4143@gmail.com>
2023-05-27 13:56:12 +0530

    added

--- bookingapp/src/ClassComponent/Parent.jsx
+++ bookingapp/src/ClassComponent/Parent.jsx
@@ -1,19 +1,9 @@
-
-import { Component } from "react";
-class Parent extends Component{
-
-    Constructor(prop){
-        super()
-    }
-    render(){
-        return( 
-            <div>
-  <h1>This  is child</h1>
-  <h1>{this.state.count</h1>
-
-            </div>
-        )
-    }
-    }
-
+const Parent = () => {
+    return ( 
+        <div>
+            Hello
+        </div>
+     );
 }
+ 
+export default Parent;
